85f9e62062bbaeec0c97a57b377f1cb4b0f86d2f
Ojomona Ethan Inedu <skidev101@gmail.com>
2025-10-18 17:27:59 +0100

    Update CORS origin for API access

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://hng-frontend.site"],
+    origin: ["http://localhost:5173", "https://me-profile-api.pxxl.click"],
   })
 );
 app.use(express.json());
